8f8adfb4c72b2697df7f2f73b694d81b9677ba11
apotek2324 <165451652+apotek2324@users.noreply.github.com>
2024-04-02 15:32:18 +0700

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,12 @@ import pandas as pd
 
 st.title(':blue[SISTEM PERIODIK UNSUR]')
 
-st.write('Tabel periodik unsur dibawah ini:')
-st.write(data_frame({
-           'IA': ['H', 'Li', 'Na', 'K', 'Rb' ,'Cs' ,'Fr'],
-}))
+#import module
+from tabulate import tabulate
+
+#assign data
+simbol = [
+           ['H', ]]
+
+#create header
+periode = ['IA', 'IIA', 'IIIB', 'IVB', 'VB', 'VIB', 'VIIB', 'VIIIB', 'VIIIB', 'IB', 'IB', 'IIB', 'IIIA', 'IVA', 'VA', 'VIA', 'VIIA', 'VIIIA']
