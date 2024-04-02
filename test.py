8d1e704d4d36a1c6044a4ee0fb4e9779d2b5e994
apotek2324 <165451652+apotek2324@users.noreply.github.com>
2024-04-02 15:36:35 +0700

    Update test.py

--- test.py
+++ test.py
@@ -3,16 +3,3 @@ import pandas as pd
 
 st.title(':blue[SISTEM PERIODIK UNSUR]')
 
-#import module
-from tabulate import tabulate
-
-#assign data
-simbol = [
-           ['H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H']
-]
-
-#create header
-golongan = ['IA', 'IIA', 'IIIB', 'IVB', 'VB', 'VIB', 'VIIB', 'VIIIB', 'VIIIB', 'IB', 'IB', 'IIB', 'IIIA', 'IVA', 'VA', 'VIA', 'VIIA', 'VIIIA']
-
-# display table
-print(tabulate(simbol, header=golongan, tablefmt="grid"))
