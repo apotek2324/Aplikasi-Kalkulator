8090c77c5cf68bbd30e13df67f0066edeaa10382
apotek2324 <165451652+apotek2324@users.noreply.github.com>
2024-04-02 15:13:00 +0700

    Update test.py

--- test.py
+++ test.py
@@ -3,5 +3,6 @@ import streamlit as st
 st.title(':blue[SISTEM PERIODIK UNSUR]')
 
 import pandas as pd
+st.write(1234567)
 st.write(pd.DataFrame({
            'IA': ['H','Li','Na','K','Rb','Cs','Fr'],
