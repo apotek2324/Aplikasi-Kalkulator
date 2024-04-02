bc657f8c23e7545377f72195b938449575b6a8c0
apotek2324 <165451652+apotek2324@users.noreply.github.com>
2024-04-02 14:43:53 +0700

    Update test.py
    1st trial

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
 import streamlit as st
 
-st.header('_Aplikasi Penjumlahan Bilangan Numerik_', divider='blue')
-
-angka_pertama = st.number_input('Masukkan angka pertama')
-st.write('The first number is', angka_pertama)
-
-angka_kedua = st.number_input('Masukkan angka kedua')
-st.write('The second number is', angka_kedua)
-
-operasi_matematika = angka_pertama * angka_kedua
-st.write(f"Angka pertama {angka_pertama} x Angka kedua {angka_kedua} = {operasi_matematika}")
+st.header('SISTEM PERIODIK UNSUR', divider='blue')
