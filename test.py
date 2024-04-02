d283952f793a9650a109882755dd8c07a0a1ccd8
apotek2324 <165451652+apotek2324@users.noreply.github.com>
2024-04-02 15:13:29 +0700

    Update test.py

--- test.py
+++ test.py
@@ -5,4 +5,4 @@ st.title(':blue[SISTEM PERIODIK UNSUR]')
 import pandas as pd
 st.write(1234567)
 st.write(pd.DataFrame({
-           'IA': ['H','Li','Na','K','Rb','Cs','Fr'],
+           'IA': ['H', 'Li', 'Na', 'K', 'Rb' ,'Cs' ,'Fr']
