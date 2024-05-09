40b37ea90806d29d419c2ea7d56ea26613a85d7d
apotek2324 <165451652+apotek2324@users.noreply.github.com>
2024-05-09 10:20:28 +0700

    Update lpk.py

--- lpk.py
+++ lpk.py
@@ -106,9 +106,6 @@ if choice == "Menghitung dengan Konsentrasi Asam Kuat":
         "Pilih senyawa asam kuat", list(asam_kuat.keys()))
     a = asam_kuat[selected_asam_kuat]
     st.write("a = ", a)
-
-    selected_asam_kuat = st.selectbox(
-        "Pilih senyawa asam kuat", list(asam_kuat()))
     if selected_asam_kuat == "Custom":
         # Masukkan konsentrasi
         Konsentrasi = st.number_input(
