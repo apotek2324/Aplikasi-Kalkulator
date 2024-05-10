521829e8f0130d7c913a249e91f511dab9053a97
apotek2324 <165451652+apotek2324@users.noreply.github.com>
2024-05-10 19:43:19 +0700

    Update lpk.py

--- lpk.py
+++ lpk.py
@@ -88,7 +88,7 @@ if choice == "Konsentrasi Asam Kuat":
     st.subheader("Menghitung pH dan [H+] dari Konsentrasi Asam Kuat")
 
     # Pilih senyawa asam kuat
-    asam_kuat = {
+    asam_kuat = [f{
         "Asam Klorida (HCl)": 1,
         "Asam Nitrat (HNO3)": 1,
         "Asam Sulfat (H2SO4)": 2,
@@ -100,28 +100,28 @@ if choice == "Konsentrasi Asam Kuat":
         "Asam Iodida (HI)": 1,
         "Asam Iodit (HIO3)": 1,
         "Asam Periodat (HIO4)": 1,
-    } + {"Custom"}
+    }] + ["Custom"]
 
     selected_asam_kuat = st.selectbox(
-        "Pilih senyawa asam kuat", list(asam_kuat.keys()))
+        "Pilih senyawa asam kuat", list(asam_kuat.keys(), asam_kuat))
     a = asam_kuat[selected_asam_kuat]
     st.write("a = ", a)
 
     if selected == "Custom":
+        # Masukkan konsentrasi
+        Konsentrasi = st.number_input(
+            "Masukkan konsentrasi (M)", format = "%.4f")
+        st.write("Konsentrasi = ", Konsentrasi)
+
+        # Masukkan valensi
+        Valensi = st.number_input(
+            "Masukkan konsentrasi (M)", format = "%.4f")
+        st.write("Valensi = ",  Valensi)
+
     # Masukkan konsentrasi
     Konsentrasi = st.number_input(
         "Masukkan konsentrasi (M)", format = "%.4f")
     st.write("Konsentrasi = ", Konsentrasi)
-
-    # Masukkan valensi
-    Valensi = st.number_input(
-        "Masukkan konsentrasi (M)", format = "%.4f")
-    st.write("Valensi = ",  Valensi)
-
-# Masukkan konsentrasi
-Konsentrasi = st.number_input(
-      "Masukkan konsentrasi (M)", format = "%.4f")
-st.write("Konsentrasi = ", Konsentrasi)
 
  # Tombol hitung
     if st.button("Hitung pH"):
