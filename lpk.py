9b4330ee49bc819dc14f68b763610cfa480015a9
apotek2324 <165451652+apotek2324@users.noreply.github.com>
2024-05-11 22:32:51 +0700

    Update lpk.py

--- lpk.py
+++ lpk.py
@@ -128,7 +128,7 @@ if choice == "Konsentrasi Asam":
         st.write("Ka = ", Konstanta_asam)
     
         # Masukkan konsentrasi
-        Konsentrasi_lemah = st.number_input('Masukkan konsentrasi (M)', format = "%.4f", step=0.0001())
+        Konsentrasi_lemah = st.number_input('Masukkan konsentrasi (M)', format = "%.4f", step=0.0001.keys())
         st.write("Konsentrasi = ", Konsentrasi_lemah)
         
         # Tombol hitung
