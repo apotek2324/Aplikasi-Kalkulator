4834ceebec30deef30e4e07f1f9d4e481fc56260
apotek2324 <165451652+apotek2324@users.noreply.github.com>
2024-05-12 07:36:07 +0700

    Update lpk.py

--- lpk.py
+++ lpk.py
@@ -406,7 +406,10 @@ elif choice == "About This App":
     #Rumus pH
     st.subheader("Rumus pH")
     st.write('pH Asam Kuat')
-    st.latex(r'''[H+] = M * a''')
+    st.latex(r'''[H+] = M x a''')
+    st.latex(r'''pH = -log [H+]''')
+    st.write('pH Asam Lemah')
+    st.latex(r'''[H+] = sqrt(M x Ka)''')
     st.latex(r'''pH = -log [H+]''')
   
     
