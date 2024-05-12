2093ebe2072d3e81f1ba4d015eb04c50e4969377
apotek2324 <165451652+apotek2324@users.noreply.github.com>
2024-05-12 08:41:12 +0700

    Update lpk.py

--- lpk.py
+++ lpk.py
@@ -3,7 +3,7 @@
 import streamlit as st
 import math
 
-st.set_page_config(page_title="Kalkulator pH", page_icon=":calculator:", layout="wide")
+st.set_page_config(page_title="Kalkulator pH", page_icon=":1234:", layout="wide")
 
 # Fungsi untuk menghitung pH asam kuat
 
